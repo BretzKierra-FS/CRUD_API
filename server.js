37035662b6b013600410c032e8f150e8fdcb8de6
BretzKierra-FS <122029593+BretzKierra-FS@users.noreply.github.com>
2023-11-04 12:30:48 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 require('dotenv').config();
-const connectDB = require('../CRUD_API/app/db/config');
+const connectDB = require('./app/db/config');
 const cors = require('cors');
 const path = require('path');
 const app = express();
-const routeHandler = require('../CRUD_API/app/routes/index');
+const routeHandler = require('./app/routes/index');
 
 connectDB();
 const PORT = process.env.PORT || 4000;
